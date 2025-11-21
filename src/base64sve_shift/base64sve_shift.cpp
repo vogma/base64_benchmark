7c1c3f6302644e4dff47c94f6576ac34fffc072a
vogma <vogel.marco@mailbox.org>
2025-11-21 16:26:00 +0100

    added sve comparison

---
+++ src/base64sve_shift/base64sve_shift.cpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include "../Base64SurveyRegistry.hpp"
+#include "base64sve/lib/include/base64sve.h"
+#include <iostream>
+
+struct BASE64SVE_Adapt
+{
+
+    static constexpr inline size_t GetDecodeExpectedLen(size_t inLen) noexcept
+    {
+        return ((inLen + 3) / 4) * 3;
+    }
+
+    static constexpr inline size_t GetEncodeLen(size_t inLen) noexcept
+    {
+        return ((inLen + 2) / 3) * 4;
+    }
+
+    static std::string encode(void (*func)(void *in, char *out, size_t inlen), const std::string &bytes)
+    {
+        size_t encLen = GetEncodeLen(bytes.length());
+        std::string encoded;
+        encoded.resize(encLen);
+        func((void *)&bytes[0], &encoded[0], bytes.length());
+        return encoded;
+    }
+
+    static std::string decode(void (*func)(char *in, void *out, size_t inlen, size_t *decoded_length), const std::string &encoded)
+    {
+        std::string decoded;
+        decoded.resize(GetDecodeExpectedLen(encoded.length()));
+        size_t dLen = 0;
+        func((char *)&encoded[0], (int8_t *)&decoded[0], (size_t)encoded.length(), &dLen);
+        // void base64_decode(char *base64_data, void *output, size_t encoded_length, size_t *decoded_length);
+        decoded.resize(dLen);
+        return decoded;
+    }
+};
+
+struct base64sve
+{
+    std::string encode(const std::string &bytes)
+    {
+        return BASE64SVE_Adapt::encode(base64sve_encode_shift, bytes);
+    }
+
+    std::string decode(const std::string &base64)
+    {
+        return BASE64SVE_Adapt::decode(base64sve_decode, base64);
+    }
+};
+
+BASE64_REGISTER_ENCODER(base64sve);
+BASE64_REGISTER_DECODER(base64sve);
